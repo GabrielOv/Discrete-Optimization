1231a5d53a1fffab3093f8c62863c7382d0597fa
GabrielOv <gabiovejero@gmail.com>
2016-12-14 16:42:03 +0100

    First steps on coloring

--- 03_coloring/solver.py
+++ 03_coloring/solver.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+import numpy as np
 
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
@@ -18,6 +18,27 @@ def solve_it(input_data):
         parts = line.split()
         edges.append((int(parts[0]), int(parts[1])))
 
+    A = np.zeros((node_count,edge_count))
+    for i in range(edge_count):
+        A[edges[i][0],i] = 1
+        A[edges[i][1],i] = -1
+
+    edges_per_node = []
+    for i in range(node_count):
+        edges_per_node.append(sum(A[i][:]))
+
+    color_vector = np.ones((node_count))*2
+    color_check  = np.dot(color_vector,A)
+
+    print(A)
+    #while any(node == 0 for node in color_check):
+    for i in range(3):
+        print(color_check)
+        max_index = edges_per_node.index(max(edges_per_node))
+        color_vector[max_index]+=1
+        color_check  = np.dot(color_vector,A)
+
+    # print(edges_per_node)
     # build a trivial solution
     # every node has its own color
     solution = range(0, node_count)
@@ -40,4 +61,3 @@ if __name__ == '__main__':
         print(solve_it(input_data))
     else:
         print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/gc_4_1)')
-
