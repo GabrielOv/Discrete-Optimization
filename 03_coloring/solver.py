24c2f1033e08bc850c6516f8303868732f3afe83
GabrielOv <gabiovejero@gmail.com>
2016-12-19 17:16:47 +0100

    Does work, but gives back suboptimal answers

--- 03_coloring/solver.py
+++ 03_coloring/solver.py
@@ -2,12 +2,18 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 
+class colorNode(object):
+    "objet representing node and the available soultion space left for it"
+    def __init__(self,index,totalNodes,conectedNodes):
+        # self.__init__()
+        self.index         = index
+        self.availableSet  = set(range(totalNodes))
+        self.conectedNodes = conectedNodes
+        self.color         = None
+        self.decided       = False
+
 def solve_it(input_data):
-    # Modify this code to run your optimization algorithm
-
-    # parse the input
     lines = input_data.split('\n')
-
     first_line = lines[0].split()
     node_count = int(first_line[0])
     edge_count = int(first_line[1])
@@ -18,37 +24,33 @@ def solve_it(input_data):
         parts = line.split()
         edges.append((int(parts[0]), int(parts[1])))
 
-    A = np.zeros((node_count,edge_count))
-    for i in range(edge_count):
-        A[edges[i][0],i] = 1
-        A[edges[i][1],i] = -1
-
-    edges_per_node = []
+    nodeList = []
     for i in range(node_count):
-        edges_per_node.append(sum(abs(A[i][:])))
-    print(edges_per_node)
-    color_vector = np.ones((node_count))
-    color_check  = np.dot(color_vector,A)
+        nodeList.append(colorNode(i,node_count,set()))
 
-    # print(A)
-    # while any(node == 0 for node in color_check):
-    for i in range(2,400):
-        # print(color_vector)
-        # print(color_check)
+    checkMetrix = np.zeros((node_count,edge_count))
+    for i in range(edge_count):
+        nodeList[edges[i][0]].conectedNodes.add(edges[i][1])
+        nodeList[edges[i][1]].conectedNodes.add(edges[i][0])
+        checkMetrix[edges[i][0]][i] =  1
+        checkMetrix[edges[i][1]][i] = -1
 
-        max_index = edges_per_node.index(max(edges_per_node))
-        edges_per_node[max_index]-=1
-        color_vector[max_index]+=1
-        color_check  = np.dot(color_vector,A)
-        print(color_check.tolist().count(0))
+    for node in nodeList:
+        if not node.decided:
+            unnavailableSet=set()
+            for neighbour in node.conectedNodes:
+                if nodeList[neighbour].decided:
+                    unnavailableSet.add(nodeList[neighbour].color)
+            node.availableSet = node.availableSet.difference(unnavailableSet)
+            node.color = min(node.availableSet)
+            node.decided=True
 
-    # print(edges_per_node)
-    # build a trivial solution
-    # every node has its own color
-    solution = map(int, color_vector.tolist())#range(0, node_count)
-    solution = list(item-1 for item in solution)
-    # prepare the solution in the specified output format
-    output_data = str(node_count) + ' ' + str(0) + '\n'
+
+    solution = [node.color for node in nodeList]
+    if 0 in (np.dot(np.array(solution),checkMetrix)):
+        print("ALAAAARM!!!!")
+
+    output_data = str(len(set(solution))) + ' ' + str(0) + '\n'
     output_data += ' '.join(map(str, solution))
 
     return output_data
@@ -60,6 +62,7 @@ if __name__ == '__main__':
     import sys
     if len(sys.argv) > 1:
         file_location = sys.argv[1].strip()
+        # file_location ="data/gc_4_1"
         with open(file_location, 'r') as input_data_file:
             input_data = input_data_file.read()
         print(solve_it(input_data))
